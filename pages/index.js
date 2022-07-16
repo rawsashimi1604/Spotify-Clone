9af2e1b54ae478f62c57c461a90ba1208ed56ad9
Gavin Loo <looweiren@gmail.com>
2022-07-17 01:44:56 +0800

    Added basic sidebar

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Sidebar from "../components/Sidebar"
 export default function Home() {
   return (
     <div className="">
-      <h1>This is a DOPE Spotify 2.0 build</h1>
-
       <main>
         <Sidebar />
         {/* Center */}
