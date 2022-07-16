06dc2ac19c126de697415d9d0892a940ada3dc3c
Gavin Loo <looweiren@gmail.com>
2022-07-17 01:32:11 +0800

    First commit...

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Sidebar from "../components/Sidebar"
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>This is a DOPE Spotify 2.0 build</h1>
+
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
